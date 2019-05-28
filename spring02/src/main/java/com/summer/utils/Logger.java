73c5a7b4cbb93e3ddb3e6026c4cf71fb9d95af58
Summer <su943515688@gmail.com>
2019-05-28 22:35:32 +0800

    [+] 四种常用通知类型

--- spring02/src/main/java/com/summer/utils/Logger.java
+++ spring02/src/main/java/com/summer/utils/Logger.java
@@ -1,7 +1,20 @@
 package com.summer.utils;
 
 public class Logger {
-    public void saveLog() {
-        System.out.println("记录日志");
+
+    public void beforeWrite() {
+        System.out.println("前置通知");
+    }
+
+    public void afterReturningWrite() {
+        System.out.println("后置通知");
+    }
+
+    public void afterThrowingWrite() {
+        System.out.println("异常通知");
+    }
+
+    public void afterWrite() {
+        System.out.println("最终通知");
     }
 }
