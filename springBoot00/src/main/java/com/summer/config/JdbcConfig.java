ca94b04ff7990c8384fb13b469a488a2ec9348c7
Summer <su943515688@gmail.com>
2019-06-18 23:58:32 +0800

    [+] springBoot 数据库yaml配置方式

--- springBoot00/src/main/java/com/summer/config/JdbcConfig.java
+++ springBoot00/src/main/java/com/summer/config/JdbcConfig.java
@@ -1,23 +1,18 @@
 package com.summer.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
 @Configuration
-@EnableConfigurationProperties(Jdbcproperties.class)
 public class JdbcConfig {
 
     @Bean("datasource")
-    public DataSource dataSource(Jdbcproperties prop) {
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setDriverClassName(prop.getDriver());
-        dataSource.setUrl(prop.getUrl());
-        dataSource.setUsername(prop.getUser());
-        dataSource.setPassword(prop.getPassword());
-        return dataSource;
+    @ConfigurationProperties("jdbc")
+    public DataSource dataSource() {
+        return new DruidDataSource();
     }
 }
