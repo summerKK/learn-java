235706fbbe20ddf1c94fa23eceaa4dcb473ff13d
Summer <su943515688@gmail.com>
2019-06-18 23:18:17 +0800

    [+] springBoot 配置数据库

--- springBoot00/src/main/java/com/summer/HelloController.java
+++ springBoot00/src/main/java/com/summer/HelloController.java
@@ -1,11 +1,17 @@
 package com.summer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.sql.DataSource;
+
 @Controller
 public class HelloController {
+
+    @Autowired
+    private DataSource datasource;
 
     @RequestMapping("hello")
     @ResponseBody
---
+++ springBoot00/src/main/java/com/summer/config/JdbcConfig.java
@@ -0,0 +1,33 @@
+package com.summer.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:jdbc.properties")
+public class JdbcConfig {
+
+    @Value("${jdbc.driver}")
+    private String jdbcDriver;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.user}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+
+    @Bean("datasource")
+    public DataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(jdbcDriver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+}
