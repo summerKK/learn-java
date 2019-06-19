021599b22a1af2990ab1e168f06c1b8fac158775
Summer <su943515688@gmail.com>
2019-06-19 23:38:29 +0800

    [+] springBoot mybatis 配置

--- springBoot00/src/main/java/com/summer/interceptor/MyInterceptor.java
+++ springBoot00/src/main/java/com/summer/interceptor/MyInterceptor.java
@@ -1,30 +1,28 @@
 package com.summer.interceptor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 public class MyInterceptor implements HandlerInterceptor {
-
-    private static final Logger logger = LoggerFactory.getLogger(MyInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.debug("preHandle method running!");
+        log.debug("preHandle method running!");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        logger.debug("postHandle method running!");
+        log.debug("postHandle method running!");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        logger.debug("afterCompletion method running!");
+        log.debug("afterCompletion method running!");
     }
 }
