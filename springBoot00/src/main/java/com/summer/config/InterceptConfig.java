41389a993c38120c43b84b61b727a968c3e19caa
Summer <su943515688@gmail.com>
2019-06-19 23:00:29 +0800

    [+] springBoot 配置拦截器

---
+++ springBoot00/src/main/java/com/summer/config/InterceptConfig.java
@@ -0,0 +1,14 @@
+package com.summer.config;
+
+import com.summer.interceptor.MyInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/**");
+    }
+}
---
+++ springBoot00/src/main/java/com/summer/interceptor/MyInterceptor.java
@@ -0,0 +1,30 @@
+package com.summer.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.debug("preHandle method running!");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.debug("postHandle method running!");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.debug("afterCompletion method running!");
+    }
+}
